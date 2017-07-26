4cf396df567119f209ef0b2e4668e4c15f161903
JakeSano <isaac.277.elifer@gmail.com>
2017-07-26 20:26:04 +0800

    Building_FieldGenerator Introduced

--- RoofConvectionEngine/Building_FieldGenerator.cs
+++ RoofConvectionEngine/Building_FieldGenerator.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using Verse;
 using RimWorld;
+using Verse;
 
 namespace StructuralFieldsPlusTesting {
-    class Building_FieldCapacitor : Building{
-        public CompFieldConduit compFieldConduit;
-        public CompFieldCapacitor compFieldCapacitor;
+    class Building_FieldGenerator : Building{
+        public CompFieldConduit compFieldConduit = new CompFieldConduit();
+        public CompFieldGenerator compFieldGenerator = new CompFieldGenerator();
 
         public int NetworkID { get => base.Map.GetComponent<FieldMap>().ConduitArray[Position.x, Position.z]; }
         public FieldNet ConnectedFieldNet { get => base.Map.GetComponent<FieldMap>().fieldNets[NetworkID]; }
@@ -17,23 +17,28 @@ namespace StructuralFieldsPlusTesting {
         public override void ExposeData() {
             base.ExposeData();
             this.compFieldConduit = base.GetComp<CompFieldConduit>();
-            this.compFieldCapacitor = base.GetComp<CompFieldCapacitor>();
+            this.compFieldGenerator = base.GetComp<CompFieldGenerator>();
         }
 
         public override void SpawnSetup(Map map, bool respawningAfterLoad) {
             base.SpawnSetup(map, respawningAfterLoad);
             this.compFieldConduit = base.GetComp<CompFieldConduit>();
-            this.compFieldCapacitor = base.GetComp<CompFieldCapacitor>();
+            this.compFieldGenerator = base.GetComp<CompFieldGenerator>();
         }
 
         public override string GetInspectString() {
+            StringBuilder stringBuilder = new StringBuilder();
 
             return "NetID: " + NetworkID.ToString() +
-                "\nAvailable Field: " + string.Format("{0:N8}", ConnectedFieldNet.AvailableField) +
-                "\nLocal Field: " + string.Format("{0:N8}", compFieldCapacitor.CurrentField) +
-                "\nMax Total Field: " + string.Format("{0:N8}", ConnectedFieldNet.maxField) +
-                "\nMax Local Field: " + string.Format("{0:N8}", compFieldCapacitor.StoredFieldMax);
+                "\nGen Per Tick Total: " + string.Format("{0:N8}", ConnectedFieldNet.GenPerTick) +
+                "\nGen Per Tick Local: " + string.Format("{0:N8}", compFieldGenerator.GenPerTick) +
+                PowerComp.CompInspectStringExtra();
             // return the complete string
+        }
+
+        public override void TickRare() {
+            base.TickRare();
+            compFieldGenerator.CompTickRareExtra();
         }
     }
 }
